3034f8e4b569f7bdec5cb47d6015fbd71d964873
sathusaikiranreddy <40272215+sathusaikiranreddy@users.noreply.github.com>
2019-09-14 20:05:42 +0530

    day 2 task

--- src/ConnectDB.java
+++ src/ConnectDB.java
@@ -1,31 +1,43 @@
-import java.sql.*;
+import java.sql.*;//importing sql 
+import java.util.Scanner;
 public class ConnectDB {
-
+//database connection variables
 	static Connection con = null;
 	
 	static String databaseName ="projectdatabase";
 	
-	static String url = "jdbc:mysql://localhost:3306/"+databaseName;
+	static String url = "jdbc:mysql://localhost:3306/"+databaseName; //url with database name
 	
-	static String username = "root";
-	static String password ="Saikiran@1729";
+	static String username = "root";//username of my db
+	static String password ="Saikiran@1729";//password of my db plz dont tell to anyone :p
 	
 	public static void main(String[] args) throws InstantiationException, IllegalAccessException, ClassNotFoundException, SQLException {
+	/*	Scanner sc = new Scanner(System.in);
+		int docterId = sc.nextInt();
+		sc.nextLine();
+		String docterName = sc.nextLine();*/
+		Class.forName("com.mysql.jdbc.Driver").newInstance();//this will say to connect mysql
 		
-		Class.forName("com.mysql.jdbc.Driver").newInstance();
-		
-		con = DriverManager.getConnection(url,username,password);
+		con = DriverManager.getConnection(url,username,password);   //creating connection with database
 		// TODO Auto-generated method stub
+		if(con!=null)
+		{
+			System.out.println(" connected");
+		}
+		else {
+			System.out.println("not connected");
+		}
+		con.close();
 		
-		PreparedStatement ps = con.prepareStatement("");
+/*		PreparedStatement ps = con.prepareStatement("INSERT INTO docter(docterID,docterName) VALUES(?,?)");// this will create statement objects to insert values into tables
 		
-		int status =ps.executeUpdate();
-		if(status !=0)
+		int status =ps.executeUpdate();//executeUpdate will do all operations of DDL
+		if(status!=0)
 		{
 			System.out.println("connected");
-		}
+		}*/
 		
 
 	}
-
+	
 }
